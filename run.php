ffd3b392938ef618dac8f396a46bb9a8b28d17de
Tomasz Dopierala <tomasz@dopierala.net.pl>
2019-01-24 20:09:04 +0000

    Daily commit

--- run.php
+++ run.php
@@ -1,6 +1,18 @@
 <?php
 
+define("_ROOTDIR_", __DIR__);
+
+require_once( __DIR__ . "/library/std.lib.php" );
 require_once( __DIR__ . "/library/NewsFeed.php" );
 
-//print_r($argv);
 $feed = new NewsFeed($argv);
+
+/**
+ * @TODO
+ * - add custom exeption class
+ * - resacale downloaded images
+ * - write properly autoload
+ * - rewrite database class for procedure use only
+ * - rewrite scripts to objected oriented
+ * - rebuild main class for difrent type of input data (for eg. html)
+ */
