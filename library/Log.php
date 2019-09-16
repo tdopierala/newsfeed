a2e8fe0df9b6d3a393813d0924973f38929ec09d
Tomasz Dopierala <tomasz.dopierala.net.pl>
2019-09-16 12:07:10 +0200

    Adding file logging

--- library/Log.php
+++ library/Log.php
@@ -2,30 +2,46 @@
 
 class Log {
 
-    private static $message;
+	private static $message;
 
-    public static function init($msg){
-        self::$message = $msg;
+	public static function init($msg){
+		self::$message = $msg;
+		self::printout($msg);
+	}
 
-        self::console($msg);
-    }
+	private static function output($message){
+		//return "\e[1;37;40m[".date("Y-m-d H:i:s") . "]\e[0m " . $message;
+		return "[".date("Y-m-d H:i:s") . "] " . $message;
+	}
 
-    public static function output($message){
-        //return date("Y-m-d H:i:s") . ": " . $mesage . "\n";
-        return "[".date("Y-m-d H:i:s") . "]: " . $message . "\n";
-    }
+	private static function toFile($msg){
+		self::$message = $msg;
+		$filename = "main_" . date("Y-m-d") . ".log";
+		$log = fopen(_ROOTDIR_ . "/log/" . $filename, "a");
+		fwrite($log, self::output($msg) . "\n");
+		fclose($log);
+	}
 
-    public static function console($msg){
-        self::$message = $msg;
-        echo self::output($msg);
-    }
+	public static function printout($msg){
+		self::$message = $msg;
+		self::toFile($msg);
+		echo "\r";
+		for ($i=0;$i<150;$i++) { echo ' '; }
+		echo "\r" . substr(self::output($msg), 0, 150) . "\n";
+	}
 
-    public static function dump($msg){
-        self::$message = $msg;
-        var_dump($msg);
-    }
+	public static function console($msg){
+		self::$message = $msg;
+		self::toFile($msg);
+		//echo "\r";
+		//for ($i=0;$i<150;$i++) { echo ' '; }
+		//echo "\r" . substr(self::output($msg), 0, 150);
+		echo "\n" . self::output($msg) . " {" . strlen(self::output($msg)) . "}";
+	}
 
-    public static function file($msg){
-        self::$message = $msg;
-    }
+	public static function dump($msg){
+		self::$message = $msg;
+		echo "\n";
+		var_dump($msg);
+	}
 }
